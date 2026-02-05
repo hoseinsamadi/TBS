e2327a024abca14b91564aac5ff7ec57e2f49d63
Hosein Samadi <husein.samadi@gmail.com>
2026-02-05 11:36:36 +0330

    در این قسمت برای آپدیت کردن و تغییر دسترسی ها

---
+++ changUserProcess.sql
@@ -0,0 +1,26 @@
+select * from [ITSDatabase].[Users].[TblProfiles]
+
+Select * from [Task].[TblTask] t
+inner join [Task].[TblWorkflowActivityInstance] a on t.WorkflowActivityInstaceID=a.WorkflowActivityInstanceID
+Where WokflowInstanceID=325236
+order by TaskID desc
+
+declare @tblTaskId int = 4010141
+begin tran
+update [Task].[TblTask]
+set UserID = 1661
+output
+    deleted.userid as oldData, inserted.userid as NewData
+where taskid = @tblTaskId
+rollback;
+
+declare @tblTaskId int = 4010141
+begin tran
+update [Task].[TblTask]
+set UserID = 1661
+output
+    deleted.userid as oldData, inserted.userid as NewData
+where taskid = @tblTaskId
+commit;
+
+
